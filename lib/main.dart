f5a454d8c2221bd33d16cd9ac85de45f0bd8f33a
Ferdaus0001 <ferdaushossan7788990@gmail.com>
2025-03-02 19:20:44 +0600

    widget

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'home_screen.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: true, // যদি শুধুমাত্র ডিবাগ মোডে চালাতে চান, তাহলে `!kReleaseMode` ব্যবহার করুন
+      builder: (context) => const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: Size(375, 812), // আপনার ডিজাইন অনুযায়ী এটি পরিবর্তন করুন
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            appBarTheme: AppBarTheme(
+              color: Colors.blue,
+              centerTitle: true,
+
+            ),
+            useMaterial3: true,
+          ),
+          home: HomeScreen(),
+          builder: DevicePreview.appBuilder, // Device Preview যোগ করা হলো
+          locale: DevicePreview.locale(context), // ভাষা সাপোর্টের জন্য
+        );
+      },
+    );
+  }
+}
